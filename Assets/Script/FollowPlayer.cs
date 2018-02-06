11783ddebf39f5bb695ff817e730a1b7d32621a4
shun <shun@fuji.local>
2018-02-06 21:50:52 +0900

    first

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform target;
+	private Vector3 offset;
+
+	void Start () 
+	{
+		offset = GetComponent<Transform> ().position + target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<Transform> ().position = target.position + offset;
+	}
+}
