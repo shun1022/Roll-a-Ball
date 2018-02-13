27dfd79a032ddab653c9d0c8e7c8fa3af2f29c62
shun <shun@fuji.local>
2018-02-13 20:57:16 +0900

    障害物に触れるとリセットする

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision Hit)
+    {
+        if(Hit.gameObject.CompareTag("Player"))
+        {
+            int SceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(SceneIndex);
+        }
+    }
+
+}
