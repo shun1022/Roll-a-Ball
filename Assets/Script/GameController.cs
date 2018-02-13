1accd2c861363129d56fa5bb0bd7cf41e73d740b
shun <shun@fuji.local>
2018-02-13 20:00:47 +0900

    アイテム残り個数表示を実装

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public UnityEngine.UI.Text ScoreLabel;
+	// Update is called once per frame
+	public void Update () 
+    {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        ScoreLabel.text = count.ToString();
+	}
+}
